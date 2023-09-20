f0fcca03753b779f4a805f6c447c85b03904e80c
valmon55 <fedor_ka@mail.ru>
2023-09-20 15:29:40 +0300

    start

---
+++ Patterns_Part2/Patterns_Part2.Final/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Patterns_Part2.Final
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
