33f4b440bca71ffd3a3d42ae87557e717a9f304b
valmon55 <fedor_ka@mail.ru>
2023-09-20 15:42:36 +0300

    + ffmeg

--- Patterns_Part2/Patterns_Part2.Final/Program.cs
+++ Patterns_Part2/Patterns_Part2.Final/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите ссылку на Youtube-видео:");
+            string? res = Console.ReadLine();
+            Console.WriteLine("Название видео и описание:");
+            Console.WriteLine("Скачиваем видео.");
         }
     }
 }
